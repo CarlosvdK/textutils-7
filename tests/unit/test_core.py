0dea80163ad114cf18dffb7d0d3f8b0aee21399d
Carlos van der Kooij <carlos.vanderkooij@alumni.esade.edu>
2025-10-29 13:11:10 +0100

    New lines of code

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -8,9 +8,15 @@ def top_n(counts, n):
     sorted_items = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
     return sorted_items[:n]
 
-#unique words (collaborative feature)
+#unique words (collaborative feature Carlos, Alex)
 def test_unique_words_basic_sorted_case_insensitive():
     assert c.unique_words("blue red RED red Blue") == ["blue", "red"]
 
 def test_unique_words_handles_punctuation_and_spaces():
     assert c.unique_words("Hi, world!! hi.") == ["hi", "world"]
+
+def test_unique_words_empty_text():
+    assert c.unique_words("") == []
+
+def test_unique_words_normalizes_apostrophes():
+    assert c.unique_words("Don't stop, dont stop!") == ["dont", "stop"]
