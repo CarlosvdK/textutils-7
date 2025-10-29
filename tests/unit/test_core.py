4ba8ee393231c8535cfbde919e588305cd42e74a
alexanderschumacher <f95fcb01@gmail.com>
2025-10-29 13:57:37 +0100

    feat(unique_words): deleting top_n function in this branch

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -4,9 +4,6 @@ def test_word_count_basic():
        text = "Red red BLUE"
        assert c.word_count(text) == {"red": 2, "blue": 1}
 
-def top_n(counts, n):
-    sorted_items = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-    return sorted_items[:n]
 
 #unique words (collaborative feature Carlos, Alex)
 def test_unique_words_basic_sorted_case_insensitive():
