d57846b3ba9f9278161b02c6d92fd12b780e1b44
Carlos van der Kooij <carlos.vanderkooij@alumni.esade.edu>
2025-10-29 11:37:13 +0100

    feat: add normalize_whitespace with tests

---
+++ src/textutils/__init__.py
@@ -0,0 +1,2 @@
+#for carlos's normalize_whitespace branch
+from .core import normalize_whitespace
--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,2 +1,7 @@
 def word_count(text):
     return 2
+
+#carlos's code for normalize whitespace
+def normalize_whitespace(text: str) -> str:
+    """Collapse runs of whitespace (spaces/tabs/newlines) to single spaces and trim edges."""
+    return " ".join(text.split())
