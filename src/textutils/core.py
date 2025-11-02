596243504bc377470d2fc9f0f9060a5919106ea6
Sara Fibla Salgado <sarafiblasalgado@gmail.com>
2025-11-02 17:36:54 +0100

    Successfully corrected word_count function

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,2 +1,9 @@
-def word_count(text):
-    return 2
+import re
+from collections import Counter
+
+def word_count(text: str) -> dict:
+    """Return case-insensitive word frequency dictionary."""
+    if not text:
+        return {}
+    words = re.findall(r"\b\w+\b", text.lower())
+    return dict(Counter(words))
