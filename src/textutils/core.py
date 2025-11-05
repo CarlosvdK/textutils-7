27436f3311554ac7fd5d1df8d4819c6a63407566
Carlos van der Kooij <carlos.vanderkooij@alumni.esade.edu>
2025-11-05 14:06:00 +0100

    explanation updated

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -3,5 +3,5 @@ def word_count(text):
 
 #carlos's code for normalize whitespace
 def normalize_whitespace(text: str) -> str:
-    """Collapse runs of whitespace (spaces/tabs/newlines) to single spaces and trim edges."""
+    """Collapse runs of whitespace (spaces/tabs/newlines)"""
     return " ".join(text.split())
